b9a891318901d77bb3531d195ace1f1c31bf809c
FarrelBeso <vabeso@up.edu.ph>
2024-06-08 16:35:10 +0800

    server update

---
+++ server/api/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import authRoutes from "../routers/auth.js";
+import adminRoutes from "../routers/admin.js";
+import customerRoutes from "../routers/customer.js";
+import reportRoutes from "../routers/report.js";
+
+// prepare the dot env
+dotenv.config({ path: "../.env" });
+// connect to express app
+const app = express();
+// middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// debugging
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+// mount the route files here
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/customer", customerRoutes);
+app.use("/report", reportRoutes);
+
+// connect to mongoDB
+const dbURI = process.env.MONGODB_URI;
+const port = process.env.PORT || 3001;
+
+mongoose
+  .connect(dbURI, {})
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server connected to port ${port} and MongoDB`);
+    });
+  })
+  .catch((error) => {
+    console.log("Unable to connect to Server and/or MongoDB", error);
+  });
