f891b42edc238fe588b1cdf2bd25d9e414aaeb5c
FarrelBeso <vabeso@up.edu.ph>
2024-06-08 18:50:41 +0800

    cors revert back

--- server/api/index.js
+++ server/api/index.js
@@ -14,33 +14,33 @@ dotenv.config({ path: "../.env" });
 const app = express();
 // middleware
 app.use(bodyParser.json());
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "https://farm-to-table-deploy-ytso.vercel.app",
-//     ],
-//     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE", "PUT"],
-//     allowedHeaders: [
-//       "X-CSRF-Token",
-//       "X-Requested-With",
-//       "Accept",
-//       "Accept-Version",
-//       "Content-Length",
-//       "Content-MD5",
-//       "Content-Type",
-//       "Date",
-//       "X-Api-Version",
-//     ],
-//     credentials: "true",
-//   })
-// );
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://farm-to-table-deploy-ytso.vercel.app",
+    ],
+    methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE", "PUT"],
+    allowedHeaders: [
+      "X-CSRF-Token",
+      "X-Requested-With",
+      "Accept",
+      "Accept-Version",
+      "Content-Length",
+      "Content-MD5",
+      "Content-Type",
+      "Date",
+      "X-Api-Version",
+    ],
+    credentials: true,
+  })
+);
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 
 // debugging
 app.get("/", (req, res) => {
