57dcdbd864f404c24abab168262c37d09a1ed95c
FarrelBeso <vabeso@up.edu.ph>
2024-06-08 18:55:21 +0800

    cors revert back

--- server/api/index.js
+++ server/api/index.js
@@ -35,12 +35,6 @@ app.use(
     credentials: true,
   })
 );
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
 
 // debugging
 app.get("/", (req, res) => {
